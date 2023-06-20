ac0c1b3f2f7158c799c0eb45833c6fc79b8fea29
Augusto <augusto.h.v.guerner@gmail.com>
2023-06-20 16:42:16 -0300

    Divisao de trabalho dos processos feita

--- Projetos/Projeto 2/main.py
+++ Projetos/Projeto 2/main.py
@@ -1,6 +1,10 @@
 import os
 import argparse
 
+from multiprocessing import Process
+
+def work_process(sudokus):
+    print(sudokus)
 
 def pos_int(value):
     pos_i = int(value)
@@ -15,22 +19,34 @@ def valid_file(file):
         raise argparse.ArgumentTypeError(msg)
     return file
 
+# Definindo os parametros do programa
 parser = argparse.ArgumentParser(add_help=True, description='Verificador de Sudoku Concorrente em Python')
-
 
 parser.add_argument('-f', '--file-name', action='store', type=valid_file, required=True, help='O nome do arquivo com as solucoes a serem validadas')
 parser.add_argument('-p', '--num-process', action='store', type=pos_int, required=True, help='O numero de processos trabalhadores')
 parser.add_argument('-t', '--num-threads', action='store', type=pos_int, required=True, help='O numero de threads de correcao a serem utilizadas por cada processo trabalhador')
 
+# Tratando eventuais erros de entrada
 try:
     args = parser.parse_args()
 except Exception as e:
     print(e)
     exit(1)
 
+# Lendo as matrizes de sudokus
 sudokus = []
 with open(args.file_name) as file:
     sudokus = [[[int(e) for e in line] for line in sudoku.split("\n")] for sudoku in file.read().split("\n\n")]
 
-print(sudokus)
+# Fazendo a divisao de trabalho das threads
+process_sudokus = [[] for _ in range(args.num_process)]
+[process_sudokus[i % args.num_process].append(sudoku) for i, sudoku in enumerate(sudokus)]
 
+# Iniciando os processos
+process = [Process(target=work_process, args=(p_s,)) for p_s in process_sudokus]
+for p in process:
+    p.start()
+
+# Esperando terminar os processos
+for p in process:
+    p.join()
