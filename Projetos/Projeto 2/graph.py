3890e6b18b13b2de60e168a785ef629aeadee1d4
AHVG <augusto.h.v.guerner@gmail.com>
2023-07-02 20:52:41 -0300

    Atualizando projeto 2

--- Projetos/Projeto 2/graph.py
+++ Projetos/Projeto 2/graph.py
@@ -3,8 +3,7 @@ import time
 import argparse
 import matplotlib.pyplot
 
-from concurrent_v1 import concurrent_solution_v1
-from concurrent_v2 import concurrent_solution_v2
+from concurrent import concurrent_solution
 from sequential import sequential_solution
 from utils import *
 
@@ -14,8 +13,8 @@ def get_sample(n_process, file, max_threads):
     y = []    
     for n_thread in range(1, max_threads + 1):
         start = time.time()
-        sys.argv = ["python", "-f", file, "-p", str(n_process), "-t", str(n_thread), "-e", "False"]
-        concurrent_solution_v2()
+        sys.argv = ["python", file, str(n_process), str(n_thread), "False"]
+        concurrent_solution()
         end = time.time()
         x.append(n_thread)
         y.append(end - start)
@@ -36,20 +35,24 @@ def graph():
         exit(1)
 
     start = time.time()
-    sys.argv = ["python", "-f", args.file_name, "-e", "False"]
+    sys.argv = ["python", args.file_name, "False"]
     sequential_solution()
     end = time.time()
-    serial_time = end - start
-    print("Tempo de referência T(1): ", serial_time)
+    sequential_time = end - start
 
-    matplotlib.pyplot.title('Gráfico speedup')
+    sudokus = len(read_sudokus(args.file_name))
+
+    print(f"Número de sudokus {sudokus}")
+    print("Tempo de referência T(1): ", sequential_time)
+
+    matplotlib.pyplot.title(f'Gráfico speedup (número de sudokus {sudokus})')
     matplotlib.pyplot.xlabel('n_threads')
     matplotlib.pyplot.ylabel('speedup')
     for n_process in range(1, args.max_process + 1):
         x, y = get_sample(n_process, args.file_name, args.max_threads)
         for n_thread, t in zip(x, y):
             print(f"Tempo T({n_process}, {n_thread}): ", t)
-        y = [serial_time / t for t in y]
+        y = [sequential_time / t for t in y]
         matplotlib.pyplot.plot(x, y, label=f'Processos: {n_process}')
         
     matplotlib.pyplot.legend()
--- Projetos/Projeto 2/sequential.py
+++ Projetos/Projeto 2/sequential.py
@@ -1,36 +1,28 @@
-import argparse
-
-from distutils.util import strtobool
+import sys
 from utils import *
 
 def worker(sudokus, enable_output):
     for i, sudoku in enumerate(sudokus):
-        blocks = get_blocks(sudoku)
         
         if enable_output:
             print(f"Processo main: resolvendo quebra-cabeças {i}")
 
-        errors = get_errors(blocks)
+        errors = get_errors(get_blocks(sudoku))
 
         if enable_output:
             print_serial_errors(errors[:])
 
 def sequential_solution():
-    parser = argparse.ArgumentParser(add_help=True, description='Verificador de Sudoku Concorrente em Python')
-
-    parser.add_argument('-f', '--file-name', action='store', type=valid_file, required=True, help='O nome do arquivo com as solucoes a serem validadas')
-    parser.add_argument('-e', '--enable_output', action="store", type=lambda x:bool(strtobool(x)), required=False, default=True,  help='Ativa ou desativa os prints')
-
-    # Tratando eventuais erros de entrada
     try:
-        args = parser.parse_args()
-    except Exception as e:
+        if len(sys.argv) not in [2,3]:
+            raise IndexError("Passou uma quantidade de argumentos diferente do esperado!")
+        file_name = valid_file(sys.argv[1])
+        enable_output = True if len(sys.argv) == 2 else valid_bool(sys.argv[2])
+    except IndexError as e:
         print(e)
-        exit(1)
+        sys.exit()
 
-    sudokus = read_sudokus(args.file_name)
-    
-    worker(sudokus, args.enable_output)
+    worker(read_sudokus(file_name), enable_output)
 
 if __name__ == "__main__":
     sequential_solution()
